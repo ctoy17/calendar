7f1efc5bbe2a29c4868f4ea59df3080ccc25c0f8
CToy17 <csharrow1994@gmail.com>
2022-06-06 16:52:35 -0700

    add calendar app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Day from "./components/Day"
 
-function App() {
+export default function App() {
+
+
+  const dates = Array.from({length: 28}, (x, i) => i + 1)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Calendar</h1>
+      <div className="Calendar-Wrapper">
+      {dates.map((today, id) => 
+        <Day key={id} today={today}/>
+      ) }
+      </div>
+
     </div>
   );
 }
-
-export default App;
